116b8b54b808efbfad02c038c859bfaa0921e73c
zouhuanxin <634448817@qq.com>
2019-10-05 21:39:54 +0800

    增加社团活动功能模块 增加更换头像功能 第八次修改 课表信息通知栏 第二次修改

--- lib/home/competition/competition_details2.dart
+++ lib/home/competition/competition_details2.dart
@@ -68,109 +68,126 @@ class competition_details2_State extends State<competition_details2> {
         centerTitle: true,
         actions: <Widget>[new Container()],
       ),
-      body: ListView(
+      body: new Column(
         children: <Widget>[
-          new Container(
-            decoration: BoxDecoration(
-                color: Colors.blue
-            ),
-            child: Row(
-              children: <Widget>[
-                Expanded(child:           Text(
-                  '\n比赛\n${cp.type}\n',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w200, fontSize: 14),textAlign: TextAlign.center,
-                ),flex: 1,),
-                Expanded(child:  Text(
-                  '\n票数\n${cp.number}\n',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w200, fontSize: 14),textAlign: TextAlign.center,
-                ),flex: 1,)
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color(int.parse('0xfff1f1f1'))
-            ),
-            child: Row(
+          Expanded(child: new Container(
+            margin: EdgeInsets.all(0),
+            padding: EdgeInsets.all(0),
+            child: ListView(
+              padding: EdgeInsets.all(0),
               children: <Widget>[
                 Container(
-                  margin: EdgeInsets.all(10.0),
-                  child: new Align(
-                    alignment: FractionalOffset.center,
-                    child: new GestureDetector(
-                      child: new ClipOval(
-                        child: new Image.memory(base64.decode(qt==null?default_image:qt.imagebase64),fit: BoxFit.fill,height: 80,width: 80,),
+                  margin: EdgeInsets.all(0),
+                  padding: EdgeInsets.all(0),
+                  decoration: BoxDecoration(
+                      color: Color(int.parse('0xfff1f1f1'))
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        child: new Align(
+                          alignment: FractionalOffset.center,
+                          child: new GestureDetector(
+                            child: new ClipOval(
+                              child: new Image.memory(base64.decode(qt==null?default_image:qt.imagebase64),fit: BoxFit.fill,height: 80,width: 80,),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      Text(
+                        qt==null?'匿名':qt.username,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 17),
+                      ),
+                    ],
                   ),
                 ),
-                Text(
-                  qt==null?'匿名':qt.username,
-                  style: TextStyle(
-                      color: Color(int.parse(color2)),
-                      fontWeight: FontWeight.w600,
-                      fontSize: 17),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.all(0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    cp.introduce,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w100,
+                        fontSize: 12),
+                    maxLines: 15,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.all(0),
+                  padding: EdgeInsets.all(5.0),
+                  child: cp.logo!=null
+                      ? new Image.network(
+                    cp.logo,
+                    fit: BoxFit.fill,
+                    width: cp.logo!=null
+                        ? MediaQueryData.fromWindow(ui.window).size.width * 0.9
+                        : 0,
+                  )
+                      : Text(''),
+                ),
+                Container(
+                  margin: EdgeInsets.all(0),
+                  padding: EdgeInsets.all(5.0),
+                  child: cp.image2!=null
+                      ? new Image.network(
+                    cp.image2,
+                    fit: BoxFit.fill,
+                    width: cp.image2!=null
+                        ? MediaQueryData.fromWindow(ui.window).size.width * 0.9
+                        : 0,
+                  )
+                      : Text(''),
+                ),
+                Container(
+                  margin: EdgeInsets.all(0),
+                  padding: EdgeInsets.all(5.0),
+                  child: cp.image3!=null
+                      ? new Image.network(
+                    cp.image3,
+                    fit: BoxFit.fill,
+                    width: cp.image3!=null
+                        ? MediaQueryData.fromWindow(ui.window).size.width * 0.9
+                        : 0,
+                  )
+                      : Text(''),
                 ),
               ],
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text(
-              cp.introduce,
-              style: TextStyle(
-                  color: Color(int.parse(color2)),
-                  fontWeight: FontWeight.w100,
-                  fontSize: 12),
-              maxLines: 15,
+          ),flex: 8,),
+          Expanded(child: new Container(
+            margin: EdgeInsets.all(0),
+            padding: EdgeInsets.all(0),
+            child: new Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(child:           Text(
+                    '\n比赛\n${cp.type}\n',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w200, fontSize: 14),textAlign: TextAlign.center,
+                  ),flex: 1,),
+                  Expanded(child:  Text(
+                    '\n票数\n${cp.number}\n',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w400, fontSize: 14),textAlign: TextAlign.center,
+                  ),flex: 1,)
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            margin: EdgeInsets.all(5.0),
-            child: cp.logo!=null
-                ? new Image.network(
-              cp.logo,
-              fit: BoxFit.fill,
-              width: cp.logo!=null
-                  ? MediaQueryData.fromWindow(ui.window).size.width * 0.9
-                  : 0,
-            )
-                : Text(''),
-          ),
-          Container(
-            margin: EdgeInsets.all(5.0),
-            child: cp.image2!=null
-                ? new Image.network(
-              cp.image2,
-              fit: BoxFit.fill,
-              width: cp.image2!=null
-                  ? MediaQueryData.fromWindow(ui.window).size.width * 0.9
-                  : 0,
-            )
-                : Text(''),
-          ),
-          Container(
-            margin: EdgeInsets.all(5.0),
-            child: cp.image3!=null
-                ? new Image.network(
-              cp.image3,
-              fit: BoxFit.fill,
-              width: cp.image3!=null
-                  ? MediaQueryData.fromWindow(ui.window).size.width * 0.9
-                  : 0,
-            )
-                : Text(''),
-          ),
+          ),flex: 1,),
         ],
-      ),
+      )
     );
   }
 }
